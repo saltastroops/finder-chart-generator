86fce38f5b9e364900f6516f97fe4a5794fdcf84
Christian Hettlage <hettlage@saao.ac.za>
2023-04-20 21:58:28 +0200

    Use Jinja2 templates

--- fcg/main.py
+++ fcg/main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
+
+from infrastructure.jinja2 import templates
 
 app = FastAPI()
 
 
 @app.get("/")
-def index() -> dict[str, str]:
-    return {"Hello": "World"}
+def index(request: Request) -> Response:
+    return templates.TemplateResponse("index.html", {"request": request})
