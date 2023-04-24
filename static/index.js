f0528e89c12f2767dc5fe659b55dc6295bb3d07e
Christian Hettlage <hettlage@saao.ac.za>
2023-04-24 19:52:41 +0200

    Implement selection between image server and custonm FITS file

--- static/index.js
+++ static/index.js
@@ -1,5 +1,10 @@
 const tabs = document.querySelectorAll("#form-tabs li");
 
+const fitsFileOptionElements = {
+  image_survey_element: document.querySelector("#image_survey_element"),
+  custom_fits_element: document.querySelector("#custom_fits_element")
+}
+
 function switchTab(event) {
   // Select the tab the user clicked on
   selectTab(event.currentTarget);
@@ -25,12 +30,37 @@ function displayTabContent(selectedTab) {
   document.getElementById(target).classList.remove("is-hidden");
 }
 
+function switchFitsOption(event) {
+  // Get the id of the FITS file option element from the data-target attribute value
+  // of the selected radio button.
+  const targetId = event.target.dataset.target;
+
+  // Select the FITS option element
+  selectFitsOption(targetId);
+}
+
+function selectFitsOption(optionElementId) {
+  // Clear any previous selection
+  const optionElementContainer = document.querySelector("#fits_option");
+  optionElementContainer.innerHTML = "";
+
+  // Add the selected FITS file option element
+  optionElementContainer.appendChild(fitsFileOptionElements[optionElementId])
+}
+
 function init() {
   // Add an event listener to all the tabs
   tabs.forEach(tab => tab.addEventListener("click", switchTab));
 
+  // Add an event listener to the radio buttons for selecting the FITS file option
+  document.querySelectorAll("#fits_controls input")
+          .forEach(input => input.addEventListener("click", switchFitsOption));
+
   // Select the first tab
   selectTab(tabs[0]);
+
+  // Select image survey as the FITS file option
+  selectFitsOption("image_survey_element");
 }
 
 init();
