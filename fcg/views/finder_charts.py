2d4e745e8c092e7c702dec08e748c841a307abad
Christian Hettlage <hettlage@saao.ac.za>
2023-07-16 21:23:36 +0200

    Fix positioning

--- fcg/views/finder_charts.py
+++ fcg/views/finder_charts.py
@@ -194,8 +194,19 @@ async def _nir(request: Request) -> Response:
             {"errors": vm.errors}, status_code=status.HTTP_400_BAD_REQUEST
         )
 
+    reference_star = (
+        SkyCoord(
+            ra=vm.reference_star_right_ascension, dec=vm.reference_star_declination
+        )
+        if vm.reference_star_right_ascension is not None
+        else None
+    )
     position = SkyCoord(ra=vm.right_ascension, dec=vm.declination)
-    survey, fits = _fits_details(vm.background_image, position)
+    if reference_star:
+        fits_center = reference_star
+    else:
+        fits_center = position
+    survey, fits = _fits_details(vm.background_image, fits_center)
     general_properties = _general_properties(
         principal_investigator=vm.principal_investigator,
         proposal_code=vm.proposal_code,
@@ -203,13 +214,6 @@ async def _nir(request: Request) -> Response:
         position=position,
         position_angle=vm.position_angle,
         survey=survey,
-    )
-    reference_star = (
-        SkyCoord(
-            ra=vm.reference_star_right_ascension, dec=vm.reference_star_declination
-        )
-        if vm.reference_star_right_ascension is not None
-        else None
     )
     finder_chart = nir_finder_chart(
         fits=fits,
@@ -269,12 +273,12 @@ def _general_properties(
 
 
 def _fits_details(
-    background_image: str | UploadFile, position: SkyCoord
+    background_image: str | UploadFile, fits_center: SkyCoord
 ) -> Tuple[str, BinaryIO]:
     if type(background_image) == str:
         survey = background_image
         return survey, load_fits(
-            survey=survey, fits_center=position, size=10 * u.arcmin
+            survey=survey, fits_center=fits_center, size=10 * u.arcmin
         )
     elif hasattr(background_image, "file"):
         survey = ""
